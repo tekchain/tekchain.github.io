8d0b57e6ef13a06f4f91c8e7201c63043cb08dac
Anamaria Gartmann <anamaria.gartmann@tekchain.co.uk>
2020-10-29 22:55:18 +0000

    fix contact form

--- v2/assets/js/main.js
+++ v2/assets/js/main.js
@@ -1,11 +1,11 @@
 console.log('here', jQuery);
 
-!(function($) {
+!(function ($) {
   "use strict";
 
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
-  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
+  $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function (e) {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       if (target.length) {
@@ -37,7 +37,7 @@ console.log('here', jQuery);
   });
 
   // Activate smooth scroll on page load with hash links in the url
-  $(document).ready(function() {
+  $(document).ready(function () {
     if (window.location.hash) {
       var initial_nav = window.location.hash;
       if ($(initial_nav).length) {
@@ -58,19 +58,19 @@ console.log('here', jQuery);
     $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
-    $(document).on('click', '.mobile-nav-toggle', function(e) {
+    $(document).on('click', '.mobile-nav-toggle', function (e) {
       $('body').toggleClass('mobile-nav-active');
       $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
       $('.mobile-nav-overly').toggle();
     });
 
-    $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
+    $(document).on('click', '.mobile-nav .drop-down > a', function (e) {
       e.preventDefault();
       $(this).next().slideToggle(300);
       $(this).parent().toggleClass('active');
     });
 
-    $(document).click(function(e) {
+    $(document).click(function (e) {
       var container = $(".mobile-nav, .mobile-nav-toggle");
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
@@ -88,10 +88,10 @@ console.log('here', jQuery);
   var nav_sections = $('section');
   var main_nav = $('.nav-menu, #mobile-nav');
 
-  $(window).on('scroll', function() {
+  $(window).on('scroll', function () {
     var cur_pos = $(this).scrollTop() + 200;
 
-    nav_sections.each(function() {
+    nav_sections.each(function () {
       var top = $(this).offset().top,
         bottom = top + $(this).outerHeight();
 
@@ -108,7 +108,7 @@ console.log('here', jQuery);
   });
 
   // Toggle .header-scrolled class to #header when page is scrolled
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('#header').addClass('header-scrolled');
     } else {
@@ -121,7 +121,7 @@ console.log('here', jQuery);
   }
 
   // Back to top button
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
       $('.back-to-top').fadeIn('slow');
     } else {
@@ -129,7 +129,7 @@ console.log('here', jQuery);
     }
   });
 
-  $('.back-to-top').click(function() {
+  $('.back-to-top').click(function () {
     $('html, body').animate({
       scrollTop: 0
     }, 1500, 'easeInOutExpo');
@@ -145,8 +145,34 @@ console.log('here', jQuery);
       mirror: false
     });
   }
-  $(window).on('load', function() {
+
+  $(window).on('load', function () {
     aos_init();
   });
 
 })(jQuery);
+
+function submitGoogleForm(form) {
+  try {
+    var data = [].slice.call(form).map(function (control) {
+      return 'value' in control && control.name ?
+        control.name + '=' + (control.value === undefined ? '' : control.value) :
+        '';
+    }).join('&');
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('POST', form.action + '/formResponse', true);
+    xhr.setRequestHeader('Accept',
+      'application/xml, text/xml, */*; q=0.01');
+    xhr.setRequestHeader('Content-type',
+      'application/x-www-form-urlencoded; charset=UTF-8');
+    xhr.send(data);
+  } catch (e) {
+  }
+
+  form.parentNode.className += ' submitted';
+
+  document.getElementById('contactForm').innerHTML="<p>Thank you for your message. You will hear from us shortly.</p>";
+
+  return false;
+}
